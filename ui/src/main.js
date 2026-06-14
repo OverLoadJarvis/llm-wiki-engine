e7e242db50ac4b98670b30cf57ab0bb6bdd9d8db
Chengcihang <19120331@bjtu.edu.cn>
2026-06-14 12:04:15 +0800

    chore: 移除旧的web和ui前端项目代码
    清理并删除了历史遗留的web和ui前端工程代码，包括所有的vue组件、配置文件、样式资源与工具代码

--- ui/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/reset.css'
-import './styles/design-system.css'
-import './styles/components.css'
-import './styles/animations.css'
-import './styles/responsive.css'
-
-createApp(App).mount('#app')
